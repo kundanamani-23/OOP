bb13b67a70e83ec0fbf3a56e6acb25f9bf67d4bd
kundanamani-23 <2420030558@klh.edu.in>
2025-07-26 23:26:59 +0530

    Practical Task-1

---
+++ Task1_Practical/src/mypackage/datatypes.java
@@ -0,0 +1,25 @@
+package mypackage;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		byte ByteVal=127;
+		short ShortVal=32767;
+		int IntVal=123456789;
+		long LongVal=1234567890L;
+		float FloatVal=3.14159265359F;
+		double DoubleVal=3.14;
+		char CharVal='I';
+		boolean Val=true;
+		
+		System.out.println("Example of a Byte Value: "+ByteVal);
+		System.out.println("Example of a Short Value: "+ShortVal);
+		System.out.println("Example of a Integer Value: "+IntVal);
+		System.out.println("Example of a Long Value: "+LongVal);
+		System.out.println("Example of a Float Value: "+FloatVal);
+		System.out.println("Example of a Double Value: "+DoubleVal);
+		System.out.println("Example of a Char Value: "+CharVal);
+		System.out.println("Example of a Boolean Value: "+Val);
+	}
+
+}
